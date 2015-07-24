6f79ffb9da14861d5216b35b484cfd153882ba81
Patrick Johanneson <pjohanneson@gmail.com>
2015-07-24 13:37:24 -0500

    THA footer hooks

--- footer.php
+++ footer.php
@@ -8,14 +8,21 @@
  */
 
 ?>
-
+	<?php tha_content_bottom(); ?>
 	</div><!-- #content -->
+	<?php tha_content_after(); ?>
 
+	<?php tha_footer_before(); ?>
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		
+	<?php tha_footer_top(); ?>
+
+
+	<?php tha_footer_bottom(); ?>	
 	</footer><!-- #colophon -->
+	<?php tha_footer_after(); ?>
 </div><!-- #page -->
 
+<?php tha_body_bottom(); ?>
 <?php wp_footer(); ?>
 
 </body>
